6f1171c6d99803c0652aed94cbd2cccd8caad0dd
barkirsh <barkirshenboim@gmail.com>
2023-01-17 03:51:42 +0200

    continue refct & serverCMD

--- ClassifyCmdServer.cpp
+++ ClassifyCmdServer.cpp
@@ -12,5 +12,10 @@ void ClassifyCmdServer :: execute(ClientData &cd){
 
 for(int i = 0 ; i< cd->test_content.size();i++){
     // apply alg on one unclassified vector from text DB and enter result to result vector
+    //initializing new dataSet (with distances)
+//    newDataSet = KnnAlg.dataSetForAlg(inputVec, dataSet, distType, k);
+//    //calling our Knn algorithm
+//    string resultKNN = KnnAlg.mostOccurType(KnnAlg.countTypesAppear(newDataSet, k));
+// result.push(resuktKNN)
 }
 }
