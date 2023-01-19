88af117b4f50b3bc3b32a279e0024668c70f9e82
barkirsh <106393592+barkirsh@users.noreply.github.com>
2023-01-20 00:07:27 +0200

    refactoring, new project

--- ServerCmd/DisplayCmdServer.cpp
+++
@@ -1,9 +0,0 @@
-//
-// Created by barki on 1/17/2023.
-//
-
-#include "../Command.h"
-DisplayCmdServer:: DisplayCmdServer(DefultIO io){
-    this->description = "display- server";
-    this->dio = io;
-}
--- ServerCmd/DownloadCmdServer.cpp
+++
@@ -1,5 +0,0 @@
-//
-// Created by barki on 1/17/2023.
-//
-
-#include "DownloadCmdServer.h"
