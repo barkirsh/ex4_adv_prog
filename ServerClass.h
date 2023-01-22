a9a3df3cb6ca4f2a3eaf2091d7c515aaf4e6086a
barkirsh <barkirshenboim@gmail.com>
2023-01-22 16:20:02 +0200

    threads for multiple clients

--- ServerClass.h
+++ ServerClass.h
@@ -6,17 +6,24 @@
 
 //#include "SocketIO.h"
 #include "CLI.h"
+#include <pthread.h>
+#include <sys/socket.h>
+#include <unistd.h>
+
 
 class ServerClass {
 private:
     const int port;
     const int sock;
+    //std::vector<std::thread> threads;
+
 public:
     explicit ServerClass(int port);
 
     void start();
-
-    void handleClient(int client_socket);
+/*
+    void * handleClient(void* client_socket);
+*/
 };
 
 
